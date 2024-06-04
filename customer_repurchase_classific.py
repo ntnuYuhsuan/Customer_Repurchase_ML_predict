409977ebd5fa3123424cf2e711880dab2784949d
Yu Hsuan <612K0028C@ntnu.edu.tw>
2024-06-04 22:54:09 +0800

    refactor: Update SMOTE oversampling for better classification performance

--- customer_repurchase_classific.py
+++ customer_repurchase_classific.py
@@ -9,6 +9,7 @@ from sklearn.tree import DecisionTreeClassifier
 from xgboost import XGBClassifier
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from imblearn.over_sampling import SMOTE
 from imblearn.under_sampling import RandomUnderSampler
 from sklearn.decomposition import TruncatedSVD
 from sklearn.pipeline import make_pipeline
@@ -17,7 +18,6 @@ from collections import Counter
 
 raw = pd.read_csv('post_PCA.csv')
 scaler = joblib.load('Models/scaler.joblib') # trained MinMaxScaler
-
 
 def feature_filter(model, df):
     # 如果模型具有係數屬性，例如邏輯迴歸
@@ -44,7 +44,6 @@ X = df.drop(['Y1_repurchase'], axis=1)
 y = df['Y1_repurchase']
 # y = df['REPUR_FLG']
 # y = df.index.isin(analysis[analysis['cluster'] == 0].index)
-
 # 標準化特徵
 # scaler = MinMaxScaler()
 # X_scaled = scaler.fit_transform(X)
@@ -53,9 +52,20 @@ X_scaled = scaler.transform(X)
 # joblib.dump(scaler, 'Models/RF_scaler.joblib')
 
 rus = RandomUnderSampler(random_state=random_state)
-X_scaled, y = rus.fit_resample(X_scaled, y)
+smote = SMOTE(sampling_strategy=0.2,random_state=random_state,)
+# smote = SMOTE(random_state=random_state)
 
-X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=random_state)
+# X_test_real, y_test_real = rus.fit_resample(X_scaled, y)
+# X_scaled, X_test, y_scaled, y_test = train_test_split(X_scaled, y, test_size=0.4, stratify=y, random_state=random_state)
+X_scaled, X_test, y_scaled, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=random_state)
+# X_test, y_test = rus.fit_resample(X_test, y_test)
+
+X_scaled, y_scaled = smote.fit_resample(X_scaled, y_scaled)
+# X_scaled, y = smote.fit_resample(X_scaled, y)
+X_train, y_train = rus.fit_resample(X_scaled, y_scaled)
+
+# X_scaled, y = rus.fit_resample(X_scaled, y)
+# X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=random_state)
 
 models = {
     'Logistic Regression': LogisticRegression(C=1000, max_iter=1000),
@@ -67,7 +77,7 @@ models = {
     # 'Decision Tree': DecisionTreeClassifier(max_depth=8, min_samples_split=2),
     'XGBoost': XGBClassifier(learning_rate=0.15, max_depth=4, n_estimators=200),
     # 'XGBoost': XGBClassifier(learning_rate=0.13, max_depth=3, n_estimators=150),
-    'SVM': SVC(C=500, gamma=0.1, kernel='linear', probability=True), # probability=True
+    # 'SVM': SVC(C=500, gamma=0.1, kernel='linear', probability=True), # probability=True
     # 'SVM': SVC(C=800, gamma=0.1, kernel='linear', probability=True),
     'SVD+Logistic Regression': make_pipeline(TruncatedSVD(n_components=50), LogisticRegression(C=1000, max_iter=1000))
 }
